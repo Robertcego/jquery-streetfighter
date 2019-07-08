52b3dd0cd7451bc72e92fe2a2ca71125cf26352f
robertcego <lrobertito@gmail.com>
2019-07-07 18:20:33 -0600

    Hadouken
    Hadouken sound has been added.

--- js/app.js
+++ js/app.js
@@ -8,10 +8,17 @@
            $(".ryu-still").show();
         })
         .mousedown(function(){
-            // Play hadouken sound
+            playHadouken(); // Play hadouken sound
             $(".ryu-ready").hide();
             $(".ryu-throwing").show();
-            $(".hadouken").show();
+            $(".hadouken").finish().show().animate (
+                {"left": "1020px"},
+                500,
+                function() {
+                    $(".hadouken").hide();
+                    $(".hadouken").css("left","625px")
+                }
+                );
             // show hadouken and the animation to the right
         })
         .mouseup(function(){
@@ -19,4 +26,9 @@
             $(".ryu-ready").show();
             // ryu goes back to the ready position
         });
+        function playHadouken() {
+            $("#hadouken-sound")[0].volume = 0.5;
+            $("#hadouken-sound")[0].load();
+            $("#hadouken-sound")[0].play();
+        }
     }); /* Last line of the script code */
